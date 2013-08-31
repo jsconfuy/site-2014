6f396e279de2e23b6208e33fbd5a0630eea7c91d
Gabriel Chertok <gchertok@gmail.com>
2013-08-31 19:56:42 -0300

    added the mongoose module and created the proposal model

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,6 @@ exports.speakers = function(req, res) {
   res.render('speakers');
 };
 
-exports.proposals = function(req, res) {
-  res.render('proposals');
-};
-
 exports.venue = function(req, res) {
   res.render('venue');
 };
