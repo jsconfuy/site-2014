ededfab58e98a03fa7ebf7f070ffef1d38b7e0a8
Pablo Ricco <pricco@gmail.com>
2013-12-13 17:50:02 -0200

    Clean html

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ exports.init = function (app) {
   app.get('/', function (req, res) {
     res.render('index', {});
   });
+  
+  app.get('/home', function (req, res) {
+    res.render('home', {});
+  });
 
   app.get('/proposals', function (req, res) {
     res.render('proposals', {});
