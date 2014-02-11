0945511b6643fbd10f79f7993f7503a28d7c1add
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2014-02-10 23:54:53 -0200

    Style details

--- public/js/base.js
+++ public/js/base.js
@@ -26,6 +26,15 @@
         $('header').removeClass('sticky');
       }
 
+      if ($(document).scrollTop() > $('.speakers').offset().top - 300) {
+        $('.speakers li:not(.active)').each(function (index) {
+          var $el = $(this);
+          window.setTimeout(function () {
+            $el.addClass('active');
+          }, index * 100);
+        });
+      }
+
       if ($(document).scrollTop() > $('.sponsors').offset().top - 300) {
         $('.sponsors li:not(.active)').each(function (index) {
           $(this).addClass('active');
