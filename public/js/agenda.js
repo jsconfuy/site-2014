ef1e079b16753f9a04575140518168153d7c0ac6
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2014-03-08 19:51:45 -0200

    Change talks descriptions event

--- public/js/agenda.js
+++ public/js/agenda.js
@@ -1,25 +1,30 @@
 (function ($) {
   'use strict';
 
+  $.fn.toggleText = function (a, b) {
+    if (this.text() === a) {
+      this.text(b);
+    } else {
+      this.text(a);
+    }
+    return this;
+  };
+
   $(document).on('ready', function () {
     $('.talk-summary, .workshop-summary').hide();
+
+    $('.talk').each(function () {
+      var btn = $('<a />').attr('href', '#').addClass('more-info').text('More information...');
+      btn = $('<small />').append(btn);
+      $(this).append(btn);
+    });
   });
 
-  $('.talk').hover(function (event) {
-    if ($(this).not('clicked'))
-      $(this).find('.talk-summary').slideToggle();
-  });
-
-  $('.talk').click(function (event) {
-    if ($(this).is('clicked')) {
-      $(this).find('.talk-summary').slideToggle();
-    }
-    $(this).toggleClass('clicked');
-  });
-
-  $('.workshop').hover(function (event) {
-    $(this).toggleClass('active');
-    $(this).find('.workshop-summary').slideToggle();
+  $(document).on('click', '.talk .more-info', function (event) {
+    var $parent = $(this).parent().parent('.talk');
+    event.preventDefault();
+    $parent.find('.talk-summary').slideToggle();
+    $(this).toggleText('More information...', 'Close');
   });
 
 }(jQuery));
